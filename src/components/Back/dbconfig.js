5840635f7f61d14cd462de615a7239f4071a3d6d
joaquinjachow <joacojachow@hotmail.com>
2024-10-03 22:11:55 -0300

    comments deleted

--- src/components/Back/dbconfig.js
+++ src/components/Back/dbconfig.js
@@ -4,27 +4,15 @@ require('dotenv').config();
 async function run() {
   try {
     const uri = process.env.MONGODB_URI;
-
-    // Verificar si el URI está definido
     if (!uri) {
       throw new Error('MONGODB_URI no está definida en el archivo .env');
     }
-
-    // Conectar a MongoDB
     await mongoose.connect(uri);
     console.log('Conectado a MongoDB');
-
-    // Hacer alguna operación, por ejemplo, ping a la base de datos
     await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log('Ping exitoso a MongoDB');
-
-    // Aquí podrías realizar operaciones con la base de datos
-    // ...
-
   } catch (error) {
     console.error('Error durante la conexión a MongoDB:', error);
   }
 }
 
-// Ejecutar la función sin desconectar inmediatamente
 run().catch(console.error);
