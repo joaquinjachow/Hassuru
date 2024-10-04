d1e4bf3ee9e882ff74ff774ac07be80243054aad
Sainte0 <s.santo2004@gmail.com>
2024-10-04 15:56:32 -0300

    agregado de carousell

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Carousell from "@/components/Carousell";
 
 export default function Home() {
   return (
     <div>
       <h1>Hassuru</h1>
+      <Carousell />
     </div>
   );
 }
