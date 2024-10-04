d0395e29b9b872f1289b7129eeb06dabdc0081cc
joaquinjachow <joacojachow@hotmail.com>
2024-10-04 00:18:53 -0300

    images added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,32 @@
 import Navbar from "@/components/NavBar";
 import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      {/* Contenedor centrado y con ancho máximo */}
+      <div className="container p-4 mx-auto">
+        {/* Flex para alinear las imágenes */}
+        <div className="flex items-center justify-between gap-x-4">
+          {/* Ajuste de las imágenes para que ocupen el ancho completo */}
+          <Image
+            src="/images/Catalogo.jpg"
+            alt="Catalogo"
+            width={620}
+            height={500}
+            className="object-cover w-[50.49%] h-auto"
+          />
+          <Image
+            src="/images/Encargo.png"
+            alt="Encargo"
+            width={600}
+            height={500}
+            className="object-cover w-[49.51%] h-auto"
+          />
+        </div>
+      </div>
     </div>
   );
 }
