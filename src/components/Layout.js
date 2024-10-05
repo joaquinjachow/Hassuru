e50812c97f255c3921065729f2569fda24eb79c7
joaquinjachow <joacojachow@hotmail.com>
2024-10-05 16:14:41 -0300

    layout with footer and navbar added

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import Navbar from './NavBar'; 
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
