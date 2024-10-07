0f83f51cddb1a6e491ed7c3da511245e2e74268d
constantino <cons_benjamin9@outlook.com>
2024-10-06 21:02:16 -0300

    agregar producto funciona, modificar productos tambien funciona (falta ver lo de la imagen) - al agregar un producto no lo muestra automaticamente

---
+++ src/components/Sidebar.js
@@ -0,0 +1,46 @@
+// components/Sidebar.js
+import React, { useState } from "react";
+import AddProductModal from './AddProductModal'; // Importa el componente del modal
+
+const Sidebar = ({ handleLogout, fetchProducts }) => {
+  const [isModalOpen, setModalOpen] = useState(false); // Estado para controlar el modal
+
+  return (
+    <aside className="w-64 bg-gray-800 text-white flex flex-col justify-between">
+      <div className="p-6">
+        <h1 className="text-3xl font-bold mb-6">Dashboard Panel</h1>
+        <ul>
+          <li className="mb-4">
+            <a 
+              href="#" 
+              onClick={(e) => {
+                e.preventDefault(); // Previene el comportamiento por defecto del enlace
+                setModalOpen(true); // Abre el modal
+              }} 
+              className="block py-2 px-4 rounded hover:bg-gray-700 transition duration-300"
+            >
+              Agregar Producto
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="p-6">
+        <button
+          onClick={handleLogout}
+          className="block py-2 px-4 rounded hover:bg-gray-700 transition duration-300 w-full text-left text-red-500"
+        >
+          Logout
+        </button>
+      </div>
+
+      {/* Integración del modal */}
+      <AddProductModal 
+        isOpen={isModalOpen} 
+        onClose={() => setModalOpen(false)} 
+        fetchProducts={fetchProducts}
+      />
+    </aside>
+  );
+};
+
+export default Sidebar;
