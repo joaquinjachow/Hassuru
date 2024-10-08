383869321027f2a7b70a2108083c07844cec7e4f
joaquinjachow <joacojachow@hotmail.com>
2024-10-08 18:05:56 -0300

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,16 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { FaInstagram, FaTiktok } from "react-icons/fa";
 
 export default function Footer() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const url = 'https://api.whatsapp.com/send?phone=3512591212&text=';
+    window.open(url, "_blank");
+};
   return (
-    <footer className="p-4 text-black bg-white border-t-2 border-black shadow-md">
-      <div className="container flex flex-col items-center justify-between mx-auto space-y-4 md:flex-row md:space-y-0">
-        {/* Logo del footer */}
-        {/* <div className="flex items-center">
-          <Image src="/logo.png" alt="Logo" width={100} height={100} />
-        </div> */}
-
-        {/* Redes sociales */}
+    <footer className="=p-4 text-black bg-white shadow-md">
+      <div className="container flex flex-col items-center justify-between mx-auto space-y-4 border-t-2 border-black rounded-sm md:flex-row md:space-y-0">
         <div className="flex space-x-6">
           <Link href="https://www.instagram.com/hassuru" aria-label="Instagram">
             <FaInstagram className="w-6 h-6 hover:text-gray-500" />
@@ -24,14 +22,14 @@ export default function Footer() {
 
         {/* Información de contacto */}
         <div className="text-sm text-center md:text-right">
-          <p>Email: hassuru.ar@gmail.com</p>
-          <p>Tel: 351 259 1212</p>
+          <p className='mt-4'>Email: hassuru.ar@gmail.com</p>
+          <button className='text-green-700' onClick={handleSubmit}>Tel: 351 259 1212</button>
         </div>
       </div>
 
       {/* Derechos reservados */}
       <div className="mt-4 text-sm text-center">
-        &copy; 2024 TuEmpresa. Todos los derechos reservados.
+        &copy; 2024 @Hassuru. Todos los derechos reservados.
       </div>
     </footer>
   );
