513836326a17249d10fdbf46273effcc8cf00b02
joaquinjachow <joacojachow@hotmail.com>
2024-10-12 16:36:41 -0300

    Image change dolar blue added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Link from "next/link";
+import useFetchDolar from "@/hooks/useFetchDolar";
 
 export default function Card({ currentProducts }) {
+  const { dolarBlue, loading, error } = useFetchDolar();
   const getDisponibilidad = (product) => {
     if (product.tallas && Object.keys(product.tallas).length > 0) {
       return { message: "Entrega inmediata", color: "text-green-500" };
@@ -9,23 +11,24 @@ export default function Card({ currentProducts }) {
       return { message: "Disponible en 15 días", color: "text-red-500" };
     }
   };
-
+  console.log(currentProducts)
   return (
     <div className="p-4"> {/* Agregado padding aquí */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
         {currentProducts.map((product, index) => {
           const disponibilidad = getDisponibilidad(product);
           return (
             <Link href={`/producto/${product._id}`} key={product.id}>
               <div key={index} className="flex flex-col justify-between h-full transition-transform transform hover:scale-105">
                 <img
-                  src={product.image?.url || "/images/default.png"}
+                  src={product.image?.base64}
                   alt={product.nombre}
                   className="object-contain w-full mb-3"
                 />
                 <h3 className="text-lg font-semibold">{product.nombre}</h3>
                 <div className="flex flex-col mt-2">
-                  <p className="text-lg font-bold text-gray-800">${product.precios.USD} USD</p>
+                  <p className="text-lg font-bold text-gray-800">${product.precio} USD</p>
+                  <p className="text-lg font-bold text-gray-800">{(product.precio * dolarBlue).toFixed(2)} ARS</p>
                   <span className={disponibilidad.color}>{disponibilidad.message}</span>
                 </div>
               </div>
