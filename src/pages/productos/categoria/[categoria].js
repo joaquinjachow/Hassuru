5336410319dc1d1a9ed2b802bd0b6c2fcfbb093d
joaquinjachow <joacojachow@hotmail.com>
2024-10-12 17:00:25 -0300

    Filters in categorys added

--- src/pages/productos/categoria/[categoria].js
+++ src/pages/productos/categoria/[categoria].js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router"
 import Card from "@/components/Card";
+import Filter from "@/components/Filtro";
 
 export default function Categoria() {
   const [products, setProducts] = useState([]);
@@ -45,32 +46,39 @@ export default function Categoria() {
   const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
 
   return (
-    <div className="container flex py-10 mx-auto">
-      <section className="w-full">
-        {loading ? (
-          <p>Cargando productos...</p>
-        ) : error ? (
-          <p className="text-red-500">Error: {error}</p>
-        ) : currentProducts.length === 0 ? (
-          <p>No hay productos disponibles en esta categoría.</p>
-        ) : (
-          <Card currentProducts={currentProducts} />
-        )}
-
-        <div className="flex justify-center mt-6">
-          {Array.from({ length: totalPages }, (_, index) => (
-            <button
-              key={index}
-              className={`px-3 py-1 rounded mx-1 ${
-                currentPage === index + 1 ? "bg-blue-500 text-white" : "bg-gray-200"
-              }`}
-              onClick={() => setCurrentPage(index + 1)}
-            >
-              {index + 1}
-            </button>
-          ))}
-        </div>
-      </section>
+<div className="container flex flex-col py-10 mx-auto lg:flex-row">
+  <aside className="w-full mb-6 lg:w-1/4 lg:mb-0">
+    <Filter 
+      products={products}
+      setFilteredProducts={setFilteredProducts}
+    />
+  </aside>
+  <section className="flex flex-col w-full lg:w-3/4">
+    {loading ? (
+      <p>Cargando productos...</p>
+    ) : error ? (
+      <p className="text-red-500">Error: {error}</p>
+    ) : currentProducts.length === 0 ? (
+      <p>No hay productos disponibles.</p>
+    ) : (
+      <Card currentProducts={currentProducts} />
+    )}
+    
+    {/* Paginado debajo de los productos */}
+    <div className="flex justify-center mt-6">
+      {Array.from({ length: totalPages }, (_, index) => (
+        <button
+          key={index}
+          className={`px-3 py-1 rounded mx-1 ${
+            currentPage === index + 1 ? "bg-blue-500 text-white" : "bg-gray-200"
+          }`}
+          onClick={() => setCurrentPage(index + 1)}
+        >
+          {index + 1}
+        </button>
+      ))}
     </div>
+  </section>
+</div>
   );
 }
