8409fb0b11f94272091d7fefcd9a3057c14da09c
constantino <cons_benjamin9@outlook.com>
2024-10-15 22:16:47 -0300

    alertas(react-hot-toast)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
   return(
   <Layout>
+    <Toaster />
     <Component {...pageProps} />
   </Layout>
   )
