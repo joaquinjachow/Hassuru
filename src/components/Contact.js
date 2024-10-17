6441ce421cb703fdcd41805b3acbe483cca1b5e0
joaquinjachow <joacojachow@hotmail.com>
2024-10-17 17:43:12 -0300

    encargo img in mobile fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -32,17 +32,15 @@ export default function Contact() {
     const inputStyle = "p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#9F1E40]";
 
     return (
-        <div className="flex flex-col items-center justify-between gap-4 md:flex-row px-4 py-20">
-            {/* Imagen de explicacion.png */}
+        <div className="flex flex-col items-center justify-between gap-4 px-4 py-20 md:flex-row">
             <div className="w-full md:w-[49.5%] h-[700px]">
                 <img
                     src="/images/explicacion.png"
                     alt="Explicación"
-                    className="object-cover w-full h-full rounded-md shadow-xl"
+                    className="object-contain w-full h-full rounded-md shadow-xl"
                 />
             </div>
 
-            {/* Formulario */}
             <div className="w-full md:w-[49.5%] h-[700px] p-8 rounded-md shadow-xl bg-gray-50 flex items-center justify-center">
                 <div className="w-full">
                     <h2 className="mb-6 text-2xl font-bold text-center">Encargos</h2>
