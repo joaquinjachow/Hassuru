e9ee00f2a979984a852e57a688e43aba502e0d1b
joaquinjachow <joacojachow@hotmail.com>
2024-10-23 16:04:00 -0300

    .

--- src/server/dbconfig.js
+++ src/server/dbconfig.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // CORS configuration
 app.use(cors({
-  origin: ['https://hassuru-production.up.railway.app/productos', "https://hassuru.vercel.app"],
+  origin: ['https://hassuru-production.up.railway.app', "https://hassuru.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
