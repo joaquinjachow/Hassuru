0d789a1213757a0c5218462f288013e5de5ba53a
joaquinjachow <joacojachow@hotmail.com>
2024-10-23 15:35:27 -0300

    cors change

--- src/server/dbconfig.js
+++ src/server/dbconfig.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://hassuru-production.up.railway.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
