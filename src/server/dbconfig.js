97e2f1ae26796cbfdc6b6a63c992c336d1f1ba41
Lucasoechsle <lukioechsle9@gmail.com>
2024-10-23 22:45:58 -0300

    hotfix_cors

--- src/server/dbconfig.js
+++ src/server/dbconfig.js
@@ -14,31 +14,21 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-
 app.use(cors({
-  origin: ['*',"https://hassuru.vercel.app"],
+  origin: "https://hassuru.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(204);
-}); 
+app.use('/api/productos', productosRoutes);
+app.use('/api/stock', stockRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/tiktoks', tiktokRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Conectado a MongoDB');
-    
-    app.use('/api/productos', productosRoutes);
-    app.use('/api/stock', stockRoutes);
-    app.use('/api/admin', adminRoutes);
-    app.use('/api/tiktoks', tiktokRoutes);
-
     app.listen(PORT, () => {
       console.log(`Backend corriendo en http://localhost:${PORT}`);
     });
